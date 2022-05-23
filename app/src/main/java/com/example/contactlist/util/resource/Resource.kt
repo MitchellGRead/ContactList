32bbcfebeed4684e6822c3822161bc372e9a363c
MitchellGRead <mitchell.g.read@gmail.com>
2022-05-23 13:27:03 -0700

    slight adjustment to error resource

--- app/src/main/java/com/example/contactlist/util/resource/Resource.kt
+++ app/src/main/java/com/example/contactlist/util/resource/Resource.kt
@@ -1,7 +1,11 @@
 package com.example.contactlist.util.resource
 
+import androidx.annotation.StringRes
+
 sealed class Resource<out T: Any> {
     data class Success<out T: Any>(val data: T) : Resource<T>()
-    data class Error(val exception: Throwable) : Resource<Nothing>()
+    data class Error(val uiText: UiText? = null, val exception: Throwable? = null) : Resource<Nothing>()
     object Loading: Resource<Nothing>()
 }
+
+data class UiText(@StringRes val text: Int)
