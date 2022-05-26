7bb1a7ef4a8aa9d865252552c6e33e8b53488e43
MitchellGRead <mitchell.g.read@gmail.com>
2022-05-25 20:39:49 -0700

    Add coroutines and test rule

---
+++ app/src/test/java/com/example/contactlist/MainCoroutineRule.kt
@@ -0,0 +1,26 @@
+package com.example.contactlist
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+class MainCoroutineRule(
+    private val dispatcher: TestDispatcher = StandardTestDispatcher()
+) : TestWatcher() {
+
+    override fun starting(description: Description) {
+        super.starting(description)
+        Dispatchers.setMain(dispatcher = dispatcher)
+    }
+
+    override fun finished(description: Description) {
+        super.finished(description)
+        Dispatchers.resetMain()
+    }
+}
